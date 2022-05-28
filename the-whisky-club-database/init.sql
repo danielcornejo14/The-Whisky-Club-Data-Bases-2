9365a445257f65cc10011327c499565862605e3a
Daniel Cornejo <47864642+danielcornejo14@users.noreply.github.com>
2022-05-27 18:12:11 -0600

    Update init.sql

--- the-whisky-club-database/init.sql
+++ the-whisky-club-database/init.sql
@@ -1,1 +1,14 @@
 -- some text
+-- HASHBYTES('MD4', 'password') funcion para guardar contraseñas
+create table Mainframe_db.dbo.Administrator
+(
+    idAdministrator int identity
+        primary key,
+    emailAddress    varchar(320)   not null,
+    userName        varchar(64)   not null,
+    password        binary(64)    not null,
+    name            varchar(64) not null,
+    lastName1       varchar(64)   not null,
+    lastName2       varchar(64)   not null,
+    status          bit default 1 not null
+)
