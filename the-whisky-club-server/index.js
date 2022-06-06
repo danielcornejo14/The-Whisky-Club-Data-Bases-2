b2916ebb08118350171d2630e28161449bbea8e1
Daniel Cornejo <47864642+danielcornejo14@users.noreply.github.com>
2022-06-06 03:19:34 +0000

    codespaces test

--- the-whisky-club-server/index.js
+++ the-whisky-club-server/index.js
@@ -11,8 +11,12 @@ app.use(bodyParser.json())
 app.use('/user', require('./users/users.controller'))
 app.use('/main-db', require('./Databases/MsSql/mainframe.controller'))
 
+app.get('/', (req,res)=>{
+    res.json({message: "this is on codespaces"})
+})
+
 const port = process.env.API_PORT
 
 app.listen(port, ()=>{
-    console.log(`server is running on port ${port}`)
+    console.log(`server is running on port http://localhost:${port}`)
 })
