73723ef26ae4b1faac33ab4c7dd70545296f5b92
Gabriel Fallas <gabrielfallas0803@gmail.com>
2022-06-08 01:09:44 -0600

    CHG insertCustomer and updateCustomer

---
+++ the-whisky-club-database/Employee_db/Procedures/replicateInsertCountry.sql
@@ -0,0 +1,25 @@
+DELIMITER //
+CREATE PROCEDURE replicateInsertCountry (
+     IN pIdCurrency int,
+     IN pName varchar(64)
+)
+BEGIN
+    IF pIdCurrency IS NOT NULL AND pName IS NOT NULL
+    THEN
+        IF ((SELECT COUNT(name) FROM country WHERE name = pName) = 0
+            AND (SELECT COUNT(idCurrency) FROM currency WHERE idCurrency = pIdCurrency
+                AND status = 1) > 0)
+        THEN
+            START TRANSACTION;
+            INSERT INTO country(idCurrency, name)
+            VALUES(pIdCountry, pIdCurrency, pName);
+            SELECT 'Country inserted.';
+            COMMIT;
+        ELSE
+            SELECT 'The country name cannot be repeated, the id must exist.';
+        END IF;
+    ELSE
+        SELECT 'Null data is not allowed.';
+    END IF;
+END //
+DELIMITER ;
