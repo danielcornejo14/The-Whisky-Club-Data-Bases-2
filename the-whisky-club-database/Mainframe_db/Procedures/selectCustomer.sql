d0d40ca10fd62ee8b589aa9f300791f66b798a4c
Andrés Eduardo Gutiérrez Salas <andreguti333@gmail.com>
2022-06-07 22:20:50 -0600

    Add total and shipping costs to customerQuery, extra values to employeeQuery, and fix productCatalog query

--- the-whisky-club-database/Mainframe_db/Procedures/selectCustomer.sql
+++ the-whisky-club-database/Mainframe_db/Procedures/selectCustomer.sql
@@ -1,10 +1,26 @@
 --this will consult on the customers with the given filters
-CREATE OR ALTER PROCEDURE selectCustomers @subscriptionType int
+CREATE OR ALTER PROCEDURE selectCustomers @subscriptionType int, @beforeDate date, @afterDate date
 WITH ENCRYPTION
 AS
 BEGIN
-    SELECT idCustomer, , name, , status
+	WITH customerSalesCTE(idCustomer, total, shippingCost, date, idShop)
+	AS
+	(
+		SELECT idCustomer, total, shippingCost, date, idShop FROM Ireland_db.dbo.WhiskeyXCustomer
+		UNION
+		SELECT idCustomer, total, shippingCost, date, idShop FROM Scotland_db.dbo.WhiskeyXCustomer
+		UNION
+		SELECT idCustomer, total, shippingCost, date, idShop FROM UnitedStates_db.dbo.WhiskeyXCustomer
+	)
+    SELECT 
+		C.idCustomer,
+		C.name,
+		C.status,
+		S.name AS subscription,
+		SUM(total) OVER(PARTITION BY C.idCustomer) AS salesTotal,
+		SUM(shippingCost) OVER(PARTITION BY C.idCustomer) AS shippingCostTotal
     FROM Customer C
 	INNER JOIN Subscription S on S.idSubscription = C.idSubscription
+	INNER JOIN customerSalesCTE CS ON CS.idCustomer = C.idCustomer
 	WHERE (@subscriptionType IS NULL OR @subscriptionType = C.idSubscription);
 END;
--- the-whisky-club-database/Mainframe_db/Procedures/selectProductCatalog.sql
+++ the-whisky-club-database/Mainframe_db/Procedures/selectProductCatalog.sql
@@ -1,20 +1,20 @@
 --this will consult on the existences of whiskeys with the given filters
-CREATE OR ALTER PROCEDURE selectProductCatalog @subscriptionType int, @shopId int
+CREATE OR ALTER PROCEDURE selectProductCatalog @idWhiskeyType int, @shopId int
 WITH ENCRYPTION
 AS
 BEGIN
-    WITH WhiskeyExistencesCTE (idWhiskeyXShop, idWhiskey, idShop)
+    WITH WhiskeyExistencesCTE (idWhiskeyXShop, idShop, idWhiskey, currentStock, status)
 	AS
 	(
-		SELECT * FROM Ireland_db.WhiskeyXShop
+		SELECT * FROM Ireland_db.dbo.WhiskeyXShop
 		UNION
-		SELECT * FROM Scotland_db.WhiskeyXShop
+		SELECT * FROM Scotland_db.dbo.WhiskeyXShop
 		UNION
-		SELECT * FROM UnitedStates_db.WhiskeyXShop
+		SELECT * FROM UnitedStates_db.dbo.WhiskeyXShop
 	)
-	SELECT idWhiskey, idWhiskeyType, status
+	SELECT WE.idWhiskey, idWhiskeyType, WE.status
     FROM WhiskeyExistencesCTE WE
-	INNER JOIN Whiskey W ON W.idWhiskey = WxS.idWiskey
+	INNER JOIN Whiskey W ON W.idWhiskey = WE.idWhiskey
 	WHERE (@idWhiskeyType IS NULL OR @idWhiskeyType = idWhiskeyType) AND
-		  (@idShop IS NULL OR @idShop = WE.idShop);
+		  (@shopId IS NULL OR @shopId = WE.idShop);
 END;
