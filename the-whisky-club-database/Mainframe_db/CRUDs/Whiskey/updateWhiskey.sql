a46eb5dd7715c8a89e6917713f099bd136484f25
Daniel Cornejo <47864642+danielcornejo14@users.noreply.github.com>
2022-06-17 00:16:15 -0600

    Update updateWhiskey.sql

--- the-whisky-club-database/Mainframe_db/CRUDs/Whiskey/updateWhiskey.sql
+++ the-whisky-club-database/Mainframe_db/CRUDs/Whiskey/updateWhiskey.sql
@@ -1,10 +1,10 @@
-CREATE PROCEDURE updateWhiskey  @idSupplier int, @idPresentation int,
-                                @idCurrency int, @idWhiskeyType int,
-                                @brand varchar(64), @price money,
-                                @alcoholContent float, @productionDate date,
-                                @dueDate date, @availability bit,
-                                @millilitersQuantity float, @whiskeyAging int,
-                                @special bit, @idWhiskey int
+CREATE OR ALTER PROCEDURE updateWhiskey @idSupplier int, @idPresentation int,
+                                        @idCurrency int, @idWhiskeyType int,
+                                        @brand varchar(64), @price money,
+                                        @alcoholContent float, @productionDate date,
+                                        @dueDate date, @availability bit,
+                                        @millilitersQuantity float, @whiskeyAging int,
+                                        @special bit, @idWhiskey int
 WITH ENCRYPTION
 AS
 BEGIN
@@ -51,23 +51,70 @@ BEGIN
                         whiskeyAging = @whiskeyAging,
                         special = @special
                     WHERE idWhiskey = @idWhiskey
-                    PRINT('Whiskey updated.')
+                    UPDATE UnitedStates_db.dbo.Whiskey
+                    SET idSupplier = @idSupplier,
+                        idPresentation = @idPresentation,
+                        idCurrency = @idCurrency,
+                        idWhiskeyType = @idWhiskeyType,
+                        brand = @brand,
+                        price = @price,
+                        alcoholContent = @alcoholContent,
+                        productionDate = @productionDate,
+                        dueDate = @dueDate,
+                        availability = @availability,
+                        millilitersQuantity = @millilitersQuantity,
+                        whiskeyAging = @whiskeyAging,
+                        special = @special
+                    WHERE idWhiskey = @idWhiskey
+                    UPDATE Scotland_db.dbo.Whiskey
+                    SET idSupplier = @idSupplier,
+                        idPresentation = @idPresentation,
+                        idCurrency = @idCurrency,
+                        idWhiskeyType = @idWhiskeyType,
+                        brand = @brand,
+                        price = @price,
+                        alcoholContent = @alcoholContent,
+                        productionDate = @productionDate,
+                        dueDate = @dueDate,
+                        availability = @availability,
+                        millilitersQuantity = @millilitersQuantity,
+                        whiskeyAging = @whiskeyAging,
+                        special = @special
+                    WHERE idWhiskey = @idWhiskey
+                    UPDATE Ireland_db.dbo.Whiskey
+                    SET idSupplier = @idSupplier,
+                        idPresentation = @idPresentation,
+                        idCurrency = @idCurrency,
+                        idWhiskeyType = @idWhiskeyType,
+                        brand = @brand,
+                        price = @price,
+                        alcoholContent = @alcoholContent,
+                        productionDate = @productionDate,
+                        dueDate = @dueDate,
+                        availability = @availability,
+                        millilitersQuantity = @millilitersQuantity,
+                        whiskeyAging = @whiskeyAging,
+                        special = @special
+                    WHERE idWhiskey = @idWhiskey
+					PRINT('Whiskey updated.')
+                    SELECT '00' AS CODE, 'Whiskey updated.' AS MESSAGE
                     COMMIT TRANSACTION
                 END TRY
                 BEGIN CATCH
                     ROLLBACK TRANSACTION
+					SELECT '01' AS CODE, 'An error has occurred in the database.' AS MESSAGE
                     RAISERROR('An error has occurred in the database.', 11, 1)
                 END CATCH
         END
         ELSE
         BEGIN
-            RAISERROR('The ids must exist, the brand name cannot be repeated, the quantity' +
-                      ' arguments must be greater than 0 and the due date must be before the' +
-                      ' production date.', 11, 1)
+			SELECT '02' AS CODE, 'The ids must exist, the brand name cannot be repeated, the quantity arguments must be greater than 0 and the due date must be before the production date.' AS MESSAGE
+            RAISERROR('The ids must exist, the brand name cannot be repeated, the quantity arguments must be greater than 0 and the due date must be before the production date.', 11, 1)
         END
     END
     ELSE
     BEGIN
+		SELECT '03' AS CODE, 'Null data is not allowed.' AS MESSAGE
         RAISERROR('Null data is not allowed.', 11, 1)
     END
 END
