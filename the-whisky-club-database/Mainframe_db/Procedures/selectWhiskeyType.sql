881209d892b4bc71a57495dd4b8817b7d8e2cc32
Daniel Cornejo <47864642+danielcornejo14@users.noreply.github.com>
2022-06-17 14:08:04 -0600

    Update selectWhiskeyType.sql

--- the-whisky-club-database/Mainframe_db/Procedures/selectWhiskeyType.sql
+++ the-whisky-club-database/Mainframe_db/Procedures/selectWhiskeyType.sql
@@ -1,7 +1,7 @@
-CREATE PROCEDURE selectWhiskeyType
+CREATE OR ALTER PROCEDURE selectWhiskeyType
 WITH ENCRYPTION
 AS
 BEGIN
     SELECT idWhiskeyType, name, status
-    FROM WhiskeyType
+    FROM WhiskeyType where status = 1
 END
