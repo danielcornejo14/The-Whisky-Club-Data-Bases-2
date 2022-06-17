8a6d119506f0f5ea048e5b8044cb9e39b611783d
Daniel Cornejo <47864642+danielcornejo14@users.noreply.github.com>
2022-06-17 14:31:05 -0600

    Update selectCurrency.sql

--- the-whisky-club-database/Mainframe_db/Procedures/selectCurrency.sql
+++ the-whisky-club-database/Mainframe_db/Procedures/selectCurrency.sql
@@ -1,7 +1,7 @@
-CREATE PROCEDURE selectCurrency
+CREATE OR ALTER PROCEDURE selectCurrency
 WITH ENCRYPTION
 AS
 BEGIN
     SELECT idCurrency, name, status
-    FROM Currency
+    FROM Currency WHERE status = 1 
 END
